abec9b83d0775aa04c9395328e8ad5dacb8a2f47
庞若男 <pangruonan@czxy.com>
2018-09-03 20:38:29 +0800

    redis测试

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
     // 定义常量
     define('ROOT', dirname(__FILE__) . '/../');
+
+    // 引入 composer 自动加载文件
+    require(ROOT.'vendor/autoload.php');
+
     function autoload($class){
         $path = str_replace('\\', '/', $class);
        
@@ -9,21 +13,31 @@
 
     spl_autoload_register('autoload');
 
-    // 添加路由
-    if(isset($_SERVER['PATH_INFO']))
-    {
-        $pathInfo = $_SERVER['PATH_INFO'];
-        // 根据 / 转成数组
-        $pathInfo = explode('/',$pathInfo);
-        // 得到控制名和方法名
-        $controller = ucfirst($pathInfo[1]).'Controller';
-        $action = $pathInfo[2];
+    // 添加路由(判断在浏览器执行还是在命令行执行)
+    if(php_sapi_name() == 'cli'){
+        $controller = ucfirst($argv[1]).'Controller';
+        $action = $argv[2];
     }
     else {
-        // 默认控制器和方法
-        $controller = 'IndexController';
-        $action = 'index';
+
+        if(isset($_SERVER['PATH_INFO']))
+        {
+            $pathInfo = $_SERVER['PATH_INFO'];
+            // 根据 / 转成数组
+            $pathInfo = explode('/',$pathInfo);
+            // 得到控制名和方法名
+            $controller = ucfirst($pathInfo[1]).'Controller';
+            $action = $pathInfo[2];
+        }
+        else 
+        {
+            // 默认控制器和方法
+            $controller = 'IndexController';
+            $action = 'index';
+        }
+
     }
+
     // 为控制器添加命名空间
     $fullController = 'controllers\\'.$controller;
 
